f3e0a0ed99e3d3ee29de6ddf37e96ecccfd7b4d7
Zealdequar <edkarpeshin@gmail.com>
2019-01-06 16:57:01 +0300

    Revert "Initial commit"
    This reverts commit bf5c1c877898fa465dfbed25f72dcfc4109977e8.

--- db/SearchingScript.sql
+++
@@ -1,9 +0,0 @@
-use TVProgBase
-go
-select ch.TitleChannel, pr.TsStartMO, pr.TsStopMO, pr.Title
-from dbo.Programmes pr
-join dbo.Channels ch
-on pr.CID = ch.ChannelID
-where pr.Title like '%Футбол%' and pr.TsStartMO > getdate()
-and pr.TID = 1
-order by pr.TsStartMO;
--- src/TVProgMvc/TVProgViewer.WebUI/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TVProgViewer.WebUI.MvcApplication" Language="C#" %>
