4ff9731f596930eec103767fff373e92c15e6107
Zealdequar <edkarpeshin@gmail.com>
2019-07-05 21:04:37 +0300

    Реализован перехват исключения CryptographicException
    Реализован перехват исключения CryptographicException

--- src/TVProgMvc/TVProgViewer.WebUI/Infrastructure/LazyGlobalist.cs
+++ src/TVProgMvc/TVProgViewer.WebUI/Infrastructure/LazyGlobalist.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Security;
 
@@ -16,24 +17,33 @@ namespace TVProgViewer.WebUI.Infrastructure
 
         public long? UserId(HttpContext httpContext)
         {
-               long userId;
-                if (httpContext.Session["ClientCode"] == null)
+            long userId;
+            if (httpContext.Session["ClientCode"] == null)
+            {
+                var authCookie = httpContext.Request.Cookies[FormsAuthentication.FormsCookieName];
+                if (authCookie != null)
                 {
-                    var authCookie = httpContext.Request.Cookies[FormsAuthentication.FormsCookieName];
-                    if (authCookie != null)
+                    try
                     {
                         FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+
                         if (authTicket != null && !authTicket.Expired)
                         {
                             var userData = authTicket.UserData.Split(',');
                             httpContext.Session["ClientCode"] = userData[1];
                         }
                     }
+                    catch (CryptographicException cex)
+                    {
+                        FormsAuthentication.SignOut();
+                        return null;
+                    }
                 }
-                if (long.TryParse(httpContext.Session["ClientCode"]?.ToString(), out userId))
-                    return userId;
+            }
+            if (long.TryParse(httpContext.Session["ClientCode"]?.ToString(), out userId))
+              return userId;
 
-                return null;
+            return null;
         }
     }
 }
