03352fa48641a85cc57327bd457fe82e9d01c6eb
Zealdequar <edkarpeshin@gmail.com>
2026-02-10 23:09:53 +0300

    Добавлены соцсети
    Добавлены социальные сети:
    • MAX
    • OK (Одноклассники)

--- src/Lib/TvProgViewer.Core/TvProgVersion.cs
+++ src/Lib/TvProgViewer.Core/TvProgVersion.cs
@@ -8,7 +8,7 @@
         /// <summary>
         /// Gets the major store version
         /// </summary>
-        public const string CURRENT_VERSION = "4.88";
+        public const string CURRENT_VERSION = "4.89";
 
         /// <summary>
         /// Gets the minor store version
